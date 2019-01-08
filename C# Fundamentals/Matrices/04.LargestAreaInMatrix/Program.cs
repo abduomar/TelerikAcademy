b7e96cc33da80f9e9667a874a28a9888e382fd65
abduomar <abduomar@rocketmail.com>
2019-01-08 23:56:29 +0200

    Remove redundant print

--- C# Fundamentals/Matrices/04.LargestAreaInMatrix/Program.cs
+++ C# Fundamentals/Matrices/04.LargestAreaInMatrix/Program.cs
@@ -6,7 +6,8 @@ namespace LargestAreaInMatrix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+            int n = int.Parse(Console.ReadLine());
         }
     }
 }

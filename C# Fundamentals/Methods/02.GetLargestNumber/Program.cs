b10236866b04db0aff656f2f7ad0430347c0c24d
abduomar <abduomar@rocketmail.com>
2019-01-12 17:11:04 +0200

    Solved With GetMax() Method

---
+++ C# Fundamentals/Methods/02.GetLargestNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace GetLargestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+             int max = int.MinValue;
+            Console.WriteLine(GetMax(numbers, max));
+        }
+        static int GetMax(int[] numbers, int max)
+        {
+            foreach (var item in numbers)
+            {
+                if(item>max)
+                {
+                    max = item;
+                }
+            }
+            return max;
+        }
+    }
+}

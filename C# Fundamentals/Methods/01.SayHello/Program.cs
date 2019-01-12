eb37adcfaf7910bff2091c88f9d4f31becd81ec4
abduomar <abduomar@rocketmail.com>
2019-01-12 16:35:50 +0200

    Solving Problems With Methods

---
+++ C# Fundamentals/Methods/01.SayHello/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SayHello
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name = Console.ReadLine();
+            PrintUserName(name);
+        }
+        static void PrintUserName(string name)
+        {
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}

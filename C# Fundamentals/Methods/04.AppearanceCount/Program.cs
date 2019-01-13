406695261ffc83ab536d7f57b49db97951d3151a
abduomar <abduomar@rocketmail.com>
2019-01-13 13:30:14 +0200

    Solved Problem Number 4

---
+++ C# Fundamentals/Methods/04.AppearanceCount/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace AppearanceCount
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+            numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int numberX = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(AppearanceCount(numbers, numberX));
+        }
+        static int AppearanceCount(int[] numbers,int x)
+        {
+            int counter = 0;
+            foreach (var item in numbers)
+            {
+                if(item==x)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+    }
+}

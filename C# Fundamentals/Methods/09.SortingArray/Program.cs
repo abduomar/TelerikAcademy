34f67aa90522027fe68354f03219b3a74ca35c0f
abduomar <abduomar@rocketmail.com>
2019-01-13 17:15:03 +0200

    Sorting Numbers In Array

---
+++ C# Fundamentals/Methods/09.SortingArray/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+namespace SortingArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+            numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(string.Join(" ", SortArray(numbers)));
+        }
+        static int[] SortArray(int[] numbers)
+        {
+            Array.Sort(numbers);
+            return numbers;
+        }
+    }
+}

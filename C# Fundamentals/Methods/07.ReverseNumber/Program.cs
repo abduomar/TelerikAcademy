e8bb880e101f20059ce7acdb8b0bbc39c616d076
abduomar <abduomar@rocketmail.com>
2019-01-13 14:12:46 +0200

    Reverse Number

---
+++ C# Fundamentals/Methods/07.ReverseNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ReverseNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            Console.WriteLine(ReverseNumber(number));
+        }
+        static string ReverseNumber(string number)
+        {
+            char[] digits = number.ToCharArray();
+            string newNumber = string.Empty;
+            for (int i = digits.Length-1; i > -1; i--)
+            {
+                newNumber += digits[i];
+            }
+            return newNumber;
+            
+        }
+    }
+}

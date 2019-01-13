d7c079aaed77a7a4899dacdc740ba376c5c4ee58
abduomar <abduomar@rocketmail.com>
2019-01-13 17:23:06 +0200

    N Factorial

---
+++ C# Fundamentals/Methods/10.NFactorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Numerics;
+
+namespace NFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(NFactorial(n));
+        }
+        static BigInteger NFactorial(int n)
+        {
+            BigInteger sum = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                sum *= i;
+            }
+            return sum;
+        }
+    }
+}

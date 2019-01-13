3b2f491ee8147c37d4db886d0844fc9b93a02fce
abduomar <abduomar@rocketmail.com>
2019-01-13 13:55:41 +0200

    Index Of First Number Larger Than Neighbours

---
+++ C# Fundamentals/Methods/06.FirstLargerThanNeighbours/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace FirstLargerThanNeighbours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+            numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(ReturnIndexOfFirstBiggestThanNeighbours(numbers));
+        }
+        static int ReturnIndexOfFirstBiggestThanNeighbours(int[] numbers)
+        {
+            int index = 0;
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (i > 0)
+                {
+                    if (numbers[i] > numbers[i + 1] && numbers[i] > numbers[i - 1])
+                    {
+                        index = i;
+                        break;
+                    }
+                    else
+                    {
+                        index = -1;
+                    }
+                }
+            }
+            return index;
+        }
+    }
+}

707250ec88e1eeec20a65dba76a3aa11e4c5e7c4
abduomar <abduomar@rocketmail.com>
2019-01-15 23:19:34 +0200

    Print Backwards And Remove All Digits

---
+++ C# Fundamentals/Strings/05.SecretMessage/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace SecretMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretMessage = Console.ReadLine();
+            char[] characters = secretMessage.ToCharArray();
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = characters.Length-1 ; i >=0; i--)
+            {
+                if(char.IsLetter(characters[i]))
+                {
+                    stringBuilder.Append(characters[i]);
+                }
+            }
+            Console.WriteLine(stringBuilder);
+        }
+        
+    }
+}

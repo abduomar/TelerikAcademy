2972e05771296ef20fbb96f656cfef1a2075a190
abduomar <abduomar@rocketmail.com>
2019-01-15 16:26:59 +0200

    Reverse A Given String

---
+++ C# Fundamentals/Strings/01.ReverseString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Console.WriteLine(ReverseString(text));
+        }
+        static string ReverseString(string text)
+        {
+            char[] ch = text.ToCharArray();
+            string newText=string.Empty;
+            for (int i = ch.Length-1; i >=0; i--)
+            {
+                newText += ch[i];
+            }
+            return newText;
+            
+        }
+    }
+}
+

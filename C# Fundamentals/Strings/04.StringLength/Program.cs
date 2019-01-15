cd7baaf01e8517ec59a1fdd1e579cb305e3b447e
abduomar <abduomar@rocketmail.com>
2019-01-15 22:59:06 +0200

    Add '*' to string if it's length is less than 20

---
+++ C# Fundamentals/Strings/04.StringLength/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StringLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int left = 0;
+            if(text.Length<20)
+            {
+                left = 20 - text.Length;
+                text+=new string('*',left);
+            }
+            Console.WriteLine(text);
+        }
+    }
+}

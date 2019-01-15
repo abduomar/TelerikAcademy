a48072f88279cad3d94cf94207b05be19422c147
abduomar <abduomar@rocketmail.com>
2019-01-15 17:07:03 +0200

    Count Of A Single Substring In A Long Text

---
+++ C# Fundamentals/Strings/03.SubstringInText/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SubstringInText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = Console.ReadLine();
+            string text = Console.ReadLine();
+            char[] ch = text.ToCharArray();
+            int counter = 0;
+            for (int i = 0; i < ch.Length-1; i++)
+            {
+                string word = (ch[i].ToString() + ch[i + 1].ToString()).ToString();
+                if(word==pattern)
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}

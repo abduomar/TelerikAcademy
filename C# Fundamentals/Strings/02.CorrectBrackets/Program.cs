514ef3d0e21f38445020e1892d517f71ac5e8a48
abduomar <abduomar@rocketmail.com>
2019-01-15 16:52:14 +0200

    Check If Brackets Are In The Correct Place

---
+++ C# Fundamentals/Strings/02.CorrectBrackets/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CorrectBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Console.WriteLine(CheckForCorrectBrackets(text));
+        }
+        static string CheckForCorrectBrackets(string text)
+        {
+            char leftBracket = '(';
+            char rightBracket = ')';
+            string result = string.Empty;
+
+            if (text.IndexOf(leftBracket) > text.IndexOf(rightBracket))
+            {
+                result = "Incorrect";
+            }
+            else
+            {
+                result = "Correct";
+            }
+            return result;
+        }
+    }
+}
